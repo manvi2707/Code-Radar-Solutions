2d46e32d1a7e3bf8065da602bed040e4e30ec974
Manvi <manvigarg2707@gmail.com>
2025-03-19 20:41:03 +0530

    Solution to Selection Sort for Integers

---
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -0,0 +1,18 @@
+#include<sdtio.h>
+int selectSort(int arr[] , int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int printArray(int arr[] , int n){
+    for(int i = 0; i<n; i++){
+        printf("%d " , arr[i]);
+    }
+}
