fc98a0e315901360283a429e0ae1d237296c77df
Manvi <manvigarg2707@gmail.com>
2025-03-19 20:59:48 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-char selectionSort(char arr[] , int n){
+#include<string.h>
+char selectionSort(char* arr[] , int n){
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n-1; j++){
-            if(arr[j][0] > arr[j+1][0]){
+            char c1 = getchar(arr[j]);
+            char c2 = getchar(arr[j+1])
+            if(c1 > c2){
                 char* temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
@@ -11,7 +14,7 @@ char selectionSort(char arr[] , int n){
     }
 }
 
-char printArray(char arr[] , int n){
+char printArray(char* arr[] , int n){
     for(int i = 0; i<n; i++){
         printf("%s " , arr[i]);
     }
