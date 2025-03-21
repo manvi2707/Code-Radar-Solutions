3ab302bb25a9648b7467d4e30ee208efbcdc4cdc
Manvi <manvigarg2707@gmail.com>
2025-03-21 12:03:16 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -3,9 +3,17 @@
 void caesarCipher(char msg[] , int shift , char enc[]){
     int l = strlen(msg);
     for(int i = 0; i<l; i++){
-        enc[i] = msg[i] + shift;
-        if(enc[i] > 122){
-            enc[i] -= 122;
+        char ch = msg[i];
+
+        if (ch >= 'a' && ch <= 'z') {  // Handle lowercase letters
+            enc[i] = 'a' + (ch - 'a' + shift) % 26;
         } 
+        else if (ch >= 'A' && ch <= 'Z') {  // Handle uppercase letters
+            enc[i] = 'A' + (ch - 'A' + shift) % 26;
+        } 
+        else {  
+            enc[i] = ch;  // Keep non-alphabet characters unchanged
+        }
     }
+    enc[l] = '\0';
 }
