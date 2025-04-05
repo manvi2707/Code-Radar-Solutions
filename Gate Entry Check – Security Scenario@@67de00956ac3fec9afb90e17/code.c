3e4f6d259b6b799b8601fbddf1a1d41211750b92
Manvi <manvigarg2707@gmail.com>
2025-04-05 14:05:28 +0530

    Solution to Gate Entry Check – Security Scenario

---
+++ Gate Entry Check – Security Scenario@@67de00956ac3fec9afb90e17/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int gateOpen(int threshold ,int entryTimes[], int n){
+    int count = 0;
+    for(int i = 0; i<n; i++){
+        if(entryTimes[i] <= 0){
+            count++;
+        }
+    }
+    if(count == threshold){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
