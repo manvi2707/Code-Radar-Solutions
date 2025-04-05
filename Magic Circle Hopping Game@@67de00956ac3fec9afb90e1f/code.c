2e1aef9bf747ad2ec4475cdd9ac2a0d7ff9697ba
Manvi <manvigarg2707@gmail.com>
2025-04-05 22:55:42 +0530

    Solution to Magic Circle Hopping Game

---
+++ Magic Circle Hopping Game@@67de00956ac3fec9afb90e1f/code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int finalEnergy(int clouds[] , int n , int k){
+    int energy = 100;
+    int l;
+    if(n-1%k == 0){
+        enegry -= 0;
+        l = n;
+    }
+    else{
+        energy -= 1;
+        l = ((n-1)/k)*k;
+    }
+    for(int i = k; i<=l; i+k){
+        if(clouds[i] == 0){
+            energy -= 1;
+        }
+        else if(clouds[i] == 1){
+            energy -= 3;
+        }
+    }
+    return enegry;
+}
