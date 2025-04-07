9d4ce2f0cbf9fd0530d01d1c57ed38cea2a751fb
Manvi <manvigarg2707@gmail.com>
2025-04-07 20:25:55 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67c9a8aa6e98bd154cc82f3d/code.c
+++ Smallest Window Sort@@67c9a8aa6e98bd154cc82f3d/code.c
@@ -1,26 +1,50 @@
 #include<stdio.h>
 int findUnsortedSubarray(int arr[] , int n){
-    int num = arr[0];
-    int count;
-    if(arr[0]>arr[2]){
-        count = 1;
+    int min = 100000;
+    int max = -100000;
+    for(int i = 0; i<n; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    int start = 0;
+    int end = 0;
+    if(arr[0]!= min){
+        start = 0;
     }
     else{
-        count = 0;
+    for(int i = 1; i<n; i++){
+        if(arr[i] != min && arr[i] == arr[i-1]){
+
+        }
+        else if(arr[i] != min){
+            start = i;
+            break;
+        }  
+        else{
+            min++;
+        }
     }
-    for(int i = 0; i<n; i++){
-        if(arr[i] == num){
-            num++;
+    }
+    if(arr[n-1]!= max){
+        end = n-1;
+    }
+    else{
+    for(int i = n-2; i>=0; i--){
+        if(arr[i]!= max && arr[i] == arr[i+1]){
+
         }
-        else if(arr[i] != num && arr[i] == arr[i-1] && (arr[i] > arr[i+2] ||arr[i]<arr[i-2])){
-            count += 1;
-        }
-        else if(arr[i] != num && arr[i] == arr[i-1]){
+        else if(arr[i]!=max){
+            end = i;
+            break;
         }
         else{
-            count += 1;
-            num++;
+            max--;
         }
     }
-    return count;
+    }
+    return(end-start+1);
 }
