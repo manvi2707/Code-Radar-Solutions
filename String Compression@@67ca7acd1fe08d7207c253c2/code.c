98209e5752cc295da1e4f6461f7c3c9af35eb344
Manvi <manvigarg2707@gmail.com>
2025-04-08 18:18:18 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -1,32 +1,38 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-char* compressString(char str[] , char compressed[]){
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* compressString(char str[], char compressed[]) {
     int l = strlen(str);
     int count = 1;
     int k = 0;
-    for(int i = 1; i<l; i++){
-        if(str[i] == str[i-1]){
+
+    for (int i = 1; i < l; i++) {
+        if (str[i] == str[i - 1]) {
             count++;
-        }
-        else{
-            if(count == 1){
-                compressed[k] = str[i-1];
-                count = 1;
-                k++;
-            }
-            else{
-                compressed[k] = str[i-1];
-                k++;
+        } else {
+            compressed[k++] = str[i - 1];
+            if (count > 1) {
                 char buffer[10];
                 sprintf(buffer, "%d", count);
                 for (int j = 0; buffer[j] != '\0'; j++) {
                     compressed[k++] = buffer[j];
-                count = 1;
+                }
             }
-            }
+            count = 1; // reset after group
         }
     }
+
+    // Process the last character/group
+    compressed[k++] = str[l - 1];
+    if (count > 1) {
+        char buffer[10];
+        sprintf(buffer, "%d", count);
+        for (int j = 0; buffer[j] != '\0'; j++) {
+            compressed[k++] = buffer[j];
+        }
+    }
+
     compressed[k] = '\0';
     return compressed;
 }
