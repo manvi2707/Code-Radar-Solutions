33d71eac35ac4e506c85dd2d09677320a2723bea
Manvi <manvigarg2707@gmail.com>
2025-04-10 22:05:10 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67c9a8aa6e98bd154cc82f3d/code.c
+++ Smallest Window Sort@@67c9a8aa6e98bd154cc82f3d/code.c
@@ -1,39 +1,26 @@
 #include<stdio.h>
 #include<limits.h>
 int findUnsortedSubarray(int arr[] , int n){
-    int max = INT_MIN;
-    int min = INT_MAX;
+    int sorted_arr[n];
     for(int i = 0; i<n; i++){
-        if(arr[i]>max){
-            max = arr[i];
-        }
-        if(arr[i]<min){
-            min = arr[i];
-        }
+        sorted_arr[i] = arr[i];
     }
-    int start = 0;
-    int end = 0;
-    if(arr[0]!=min){
-        start = 0;
-    }
-    else{
-    for(int i = 1; i<n; i++){
-        if(arr[i]<arr[i-1]){
-            start = i-1; 
-            break;
-        }
-    }}
-    if(arr[n-1]!=max){
-        end = n-1;
-    }
-    else{
-        for(int i = n-1; i>0; i++){
-            if(arr[i-1]>arr[i]){
-                end = i;
-                break;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(sorted_arr[i]>sorted_arr[j]){
+                int temp = sorted_arr[i];
+                sorted_arr[i] = sorted_arr[j];
+                sorted_arr[j] = temp;
             }
         }
     }
-    int l = end-start+1;
-    return l;
+    int count = 0;
+    for(int i = 0; i<n; i++){
+        printf("%d %d\n" , arr[i],sorted_arr[i] );
+    }
+    // for(int i = 0; i<n; i++){
+    //     if(sorted_arr[i]!=arr[i]){
+    //         count++;
+    //     }
+    // }
 }
