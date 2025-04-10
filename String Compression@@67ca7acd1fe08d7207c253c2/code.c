ce319e0165e7f50e7dffc26e51958f03dd99cd8b
Manvi <manvigarg2707@gmail.com>
2025-04-10 17:42:29 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -29,7 +29,9 @@ char* compressString(char str[], char compressed[]) {
         }
     }
     compressed[k] = '\0';
+    printf("%d" , l);
     int comp_l = strlen(compressed);
+    printf("%d" , comp_l);
     if(l == comp_l){
         for(int i = 1;i<=l;i++){
             if(compressed[i]=='2'){
